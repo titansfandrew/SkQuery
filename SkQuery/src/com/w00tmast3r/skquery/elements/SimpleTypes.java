a286e6ed444b02fe533eb0db0aaf034ef7102945
Sean Grover <Sean Grover@Gladiator>
2017-10-08 14:53:12 -0600

    SkQuery Update 3.5.9-Lime

---
+++ SkQuery/src/com/w00tmast3r/skquery/elements/SimpleTypes.java
@@ -0,0 +1,28 @@
+package com.w00tmast3r.skquery.elements;
+
+import com.w00tmast3r.skquery.api.AbstractTask;
+import com.w00tmast3r.skquery.skript.EnumClassInfo;
+import com.w00tmast3r.skquery.util.minecraft.MoonPhase;
+import org.bukkit.Art;
+import org.bukkit.FireworkEffect;
+import org.bukkit.Sound;
+import org.bukkit.entity.Villager;
+import org.bukkit.event.entity.EntityRegainHealthEvent;
+import org.bukkit.event.inventory.InventoryType;
+import org.bukkit.scoreboard.DisplaySlot;
+
+
+public class SimpleTypes extends AbstractTask {
+
+    @Override
+    public void run() {
+        EnumClassInfo.create(DisplaySlot.class, "displayslot").register();
+        EnumClassInfo.create(Sound.class, "sound").register();
+        EnumClassInfo.create(FireworkEffect.Type.class, "fireworktype").after("entitytype").register();
+        EnumClassInfo.create(InventoryType.class, "inventorytype").after("gamemode", "entitytype").register();
+        EnumClassInfo.create(Villager.Profession.class, "profession").register();
+        EnumClassInfo.create(Art.class, "art").after("damagecause", "entitytype").register();
+        EnumClassInfo.create(MoonPhase.class, "moonphase").register();
+        EnumClassInfo.create(EntityRegainHealthEvent.RegainReason.class, "regainreason").after("potioneffecttype").register();
+    }
+}
